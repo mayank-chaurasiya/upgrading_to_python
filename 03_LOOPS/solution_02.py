fd649a38f7e4a23cdfec9a19b4fda7a43a8cdd38
mayank-chaurasiya <try.maynk@gmail.com>
2026-04-15 02:04:08 +0530

    Sum of even numbers

--- 03_LOOPS/solution_02.py
+++ 03_LOOPS/solution_02.py
@@ -5,8 +5,8 @@ Problem: Calculate the sum of even numbers up to a given number n.
 n = int(input("Enter your number: "))
 sum_even = 0
 
-for num in range(n):
-    if num % 2 == 0:
-        sum_even += num
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        sum_even += i
 
 print(f"sum of even numbers up to {n} is {sum_even}")
