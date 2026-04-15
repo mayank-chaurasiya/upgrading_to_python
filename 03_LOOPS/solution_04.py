aaa2ed76fa79daaf936e7e5303e307f4c81003dc
mayank-chaurasiya <try.maynk@gmail.com>
2026-04-15 13:11:25 +0530

    Reverse a String

---
+++ 03_LOOPS/solution_04.py
@@ -0,0 +1,11 @@
+"""Reverse a String
+Problem: Reverse a string using a loop.
+"""
+
+input_str = input("Your string: ")
+reversed_str = ""
+
+for char in input_str:
+    reversed_str = char + reversed_str
+
+print(reversed_str)
