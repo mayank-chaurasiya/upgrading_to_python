ffa0c6ccf808ed52caac39f6839d91cba0edece8
mayank-chaurasiya <try.maynk@gmail.com>
2026-04-16 01:32:18 +0530

    Function to multiply numbers or strings

---
+++ 04_FUNCTIONS/solution_03.py
@@ -0,0 +1,13 @@
+"""Problem:
+Write a function multiply that multiplies two numbers,
+but can also accept and multiply strings."""
+
+
+def multiply(p1, p2):
+    """Return multiply of two numbers or strings"""
+    return p1 * p2
+
+
+print(multiply(9, 5))
+print(multiply("a", 5))
+print(multiply(5, "a"))
