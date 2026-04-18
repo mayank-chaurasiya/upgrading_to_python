af7a65454b8d29b37ece9b0467b99dbc5a8a71c0
mayank-chaurasiya <try.maynk@gmail.com>
2026-04-19 01:51:07 +0530

    Function with key: value arguments

---
+++ 04_FUNCTIONS/solution_08.py
@@ -0,0 +1,15 @@
+"""Function with **kwargs
+Problem: Create a function that accepts any number of keyword arguments
+and prints them in the format key: value.
+"""
+
+
+def print_kwargs(**kwargs):
+    """print function in key: value format"""
+    for key, value in kwargs.items():
+        print(f"{key}: {value}")
+
+
+print_kwargs(name="shaktiman", power="lazer")
+print_kwargs(name="shaktiman")
+print_kwargs(name="shaktiman", power="lazer", enemy="Dr. Jackaal")
